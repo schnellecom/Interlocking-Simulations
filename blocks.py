247747d4d3734f1cb9063d130fee848c74c4c412
Lukas Schnelle <lukas.schnelle1@rwth-aachen.de>
2023-07-24 10:17:11 +0200

    first working job creation

--- blocks.py
+++ blocks.py
@@ -72,5 +72,78 @@ mdb.models['two-cubes'].interactionProperties['IntProp-1'].NormalBehavior(
     pressureOverclosure=HARD, allowSeparation=ON,
     constraintEnforcementMethod=DEFAULT)
 
+# delete old part
+del mdb.models['two-cubes'].parts['PART-1']
 
+# make assembly
+a2 = mdb.models['two-cubes'].rootAssembly
+p = mdb.models['two-cubes'].parts['PART-1_geom']
+a2.Instance(name='PART-1_geom-1', part=p, dependent=ON)
 
+# create load
+a1 = mdb.models['two-cubes'].rootAssembly
+s1 = a1.instances['PART-1_geom-1'].faces
+side1Faces1 = s1.findAt(((3.333333, 6.666667, 10.001), ), ((6.666667, 3.333333,
+    10.001), ))
+region = a1.Surface(side1Faces=side1Faces1, name='Surf-6')
+mdb.models['two-cubes'].Pressure(name='Load-2', createStepName='Step-1',
+    region=region, distributionType=UNIFORM, field='', magnitude=100.0,
+    amplitude=UNSET)
+
+# boundary conditions
+a1 = mdb.models['two-cubes'].rootAssembly
+v1 = a1.instances['PART-1_geom-1'].vertices
+verts1 = v1.findAt(((0.0, 10.0, 0.0), ), ((0.0, 0.0, 0.0), ), ((10.0, 0.0,
+    0.0), ), ((10.0, 10.0, 0.0), ))
+region = a1.Set(vertices=verts1, name='Set-2')
+mdb.models['two-cubes'].EncastreBC(name='BC-2', createStepName='Step-1',
+    region=region, localCsys=None)
+
+# mesh part
+p = mdb.models['two-cubes'].parts['PART-1_geom']
+p.seedPart(size=1.0, deviationFactor=0.1, minSizeFactor=0.1)
+
+# change to tet mesh
+p = mdb.models['two-cubes'].parts['PART-1_geom']
+c = p.cells
+pickedRegions = c.findAt(((6.666667, 3.333333, 5.0), ), ((6.666667, 3.333333,
+    10.001), ))
+p.setMeshControls(regions=pickedRegions, elemShape=TET, technique=FREE)
+elemType1 = mesh.ElemType(elemCode=C3D20R)
+elemType2 = mesh.ElemType(elemCode=C3D15)
+elemType3 = mesh.ElemType(elemCode=C3D10)
+p = mdb.models['two-cubes'].parts['PART-1_geom']
+c = p.cells
+cells = c.findAt(((6.666667, 3.333333, 5.0), ), ((6.666667, 3.333333, 10.001),
+    ))
+pickedRegions =(cells, )
+p.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2,
+    elemType3))
+
+# mesh itself
+p = mdb.models['two-cubes'].parts['PART-1_geom']
+p.generateMesh()
+a = mdb.models['two-cubes'].rootAssembly
+session.viewports['Viewport: 1'].setValues(displayedObject=a)
+a1 = mdb.models['two-cubes'].rootAssembly
+a1.regenerate()
+session.viewports['Viewport: 1'].assemblyDisplay.setValues(mesh=ON,
+    optimizationTasks=OFF, geometricRestrictions=OFF, stopConditions=OFF)
+session.viewports['Viewport: 1'].assemblyDisplay.meshOptions.setValues(
+    meshTechnique=ON)
+p = mdb.models['two-cubes'].parts['PART-1_geom']
+session.viewports['Viewport: 1'].setValues(displayedObject=p)
+session.viewports['Viewport: 1'].partDisplay.setValues(mesh=ON)
+session.viewports['Viewport: 1'].partDisplay.meshOptions.setValues(
+    meshTechnique=ON)
+session.viewports['Viewport: 1'].partDisplay.geometryOptions.setValues(
+    referenceRepresentation=OFF)
+
+# create job
+mdb.Job(name='script-test', model='two-cubes', description='', type=ANALYSIS,
+    atTime=None, waitMinutes=0, waitHours=0, queue=None, memory=90,
+    memoryUnits=PERCENTAGE, getMemoryFromAnalysis=True,
+    explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF,
+    modelPrint=OFF, contactPrint=OFF, historyPrint=OFF, userSubroutine='',
+    scratch='', resultsFormat=ODB, numThreadsPerMpiProcess=1,
+    multiprocessingMode=DEFAULT, numCpus=1, numGPUs=0)
